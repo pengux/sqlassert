ea405cfce4ee609effc574c48bd1396f10f7ff9f
Peter Nguyen <peter@mictis.com>
2022-06-16 22:18:50 +0200

    add tests for error cases

--- postgres_test.go
+++ postgres_test.go
@@ -32,6 +32,48 @@ func TestPostgresAsserter(t *testing.T) {
 	pgasserter.IndexNotExists(t, table, nonExisting)
 }
 
+func TestPostgresAsserterError(t *testing.T) {
+	pgasserter := sqlassert.NewPostgresAsserter(getDB())
+	mockT := new(mockT)
+
+	table := "sqlassert_test"
+	column := "sku"
+	constraint := "sqlassert_test_pkey"
+	row := map[string]interface{}{"sku": "sku1", "name": "name1"}
+	index := "sqlassert_test_name_idx"
+	nonExisting := "non_existing"
+
+	pgasserter.TableExists(mockT, nonExisting)
+	mockT.expectLastError(t, "table "+nonExisting+" does not exist")
+
+	pgasserter.ColumnExists(mockT, table, nonExisting)
+	mockT.expectLastError(t, "column "+nonExisting+" does not exist in table "+table)
+
+	pgasserter.ConstraintExists(mockT, table, nonExisting)
+	mockT.expectLastError(t, "constraint "+nonExisting+" does not exist in table "+table)
+
+	pgasserter.RowExists(mockT, table, map[string]interface{}{"sku": nonExisting})
+	mockT.expectLastError(t, "row with criteria map[sku:"+nonExisting+"] does not exist in table "+table)
+
+	pgasserter.IndexExists(mockT, table, nonExisting)
+	mockT.expectLastError(t, "index "+nonExisting+" does not exist in table "+table)
+
+	pgasserter.TableNotExists(mockT, table)
+	mockT.expectLastError(t, "table "+table+" exists")
+
+	pgasserter.ColumnNotExists(mockT, table, column)
+	mockT.expectLastError(t, "column "+column+" exists in table "+table)
+
+	pgasserter.ConstraintNotExists(mockT, table, constraint)
+	mockT.expectLastError(t, "constraint "+constraint+" exists in table "+table)
+
+	pgasserter.RowNotExists(mockT, table, row)
+	mockT.expectLastError(t, "row with criteria map[name:name1 sku:sku1] exists in table "+table)
+
+	pgasserter.IndexNotExists(mockT, table, index)
+	mockT.expectLastError(t, "index "+index+" exists in table "+table)
+}
+
 func TestPostgresAsserterPanic(t *testing.T) {
 	db, mock, err := sqlmock.New()
 	if err != nil {
--- sqlassert_test.go
+++ sqlassert_test.go
@@ -2,11 +2,31 @@ package sqlassert_test
 
 import (
 	"database/sql"
+	"fmt"
 	"os"
 	"testing"
 
 	_ "github.com/jackc/pgx/v4/stdlib"
 )
+
+type mockT []string
+
+func (mt *mockT) Errorf(format string, args ...interface{}) {
+	*mt = append(*mt, fmt.Sprintf(format, args...))
+}
+
+func (mt *mockT) lastError() string {
+	if len(*mt) == 0 {
+		return ""
+	}
+	return (*mt)[len(*mt)-1]
+}
+
+func (mt *mockT) expectLastError(t *testing.T, msg string) {
+	if mt.lastError() != msg {
+		t.Errorf("expected error '%s', got '%s'", msg, mt.lastError())
+	}
+}
 
 func getDB() *sql.DB {
 	db, err := sql.Open("pgx", os.Getenv("POSTGRES_DB_URL"))
